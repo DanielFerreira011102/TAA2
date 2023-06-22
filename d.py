1eee1bb2f3dab1a4a39cf672f652bf6bf6a6d495
Xela10001 <alexandre.gazur@ua.pt>
2023-06-22 23:41:29 +0100

    gg

---
+++ d.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tabulate import tabulate
+
+data = pd.read_csv('WVS_Cross-National_Wave_7_csv_v5_0_social_religious_ethical.csv', nrows=100)
+
+
+print(data)
